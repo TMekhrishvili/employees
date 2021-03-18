3710c6b3a50442c36bcf495a048151214d9d2577
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-18 14:11:04 +0400

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './index.css'
 
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="main">
+      <h1>Some Form:</h1>
+      <form>
+        <input type="text" name="firstname" placeholder="Firstname" />
+        <input type="text" name="lastname" placeholder="Lastname" />
+        <button type="submit">Send</button>
+      </form>
     </div>
   )
 }
