d19b327adc4ec07be64d2d3941c6b599f05b6d58
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-19 22:42:13 +0400

    sqlite

--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3').verbose()
 
-app.get('/', (req, res)=>{
-    res.send("Hello My World")
+const db = new sqlite3.Database('Users')
+
+
+let arr = [
+    [1, "Temur", "Chkadua"],
+    [2, "Vakhtang", "Kvaratskhelia"],
+    [3, "Bakur", "Buchukuri"],
+    [4, "Malkhaz", "Ashordia"],
+    [5, "Nestan", "Kekelia"],
+];
+
+db.serialize(() => {
+    db.run("CREATE TABLE users (id INT, Firstname TEXT, Lastname TEXT)")
+    var stmt = db.prepare("INSERT INTO user VALUES (?,?,?)");
+    for (var i = 0; i < arr.length; i++) {
+        stmt.run(arr[i], error => {
+            if (error) throw error
+        })
+    }
+    stmt.finalize();
+})
+var str = ""
+db.each("SELECT id, Firstname FROM users", function (err, row) {
+    str += (row.id + ". " + row.Firstname + " " + row.Lastname + ". ")
 })
 
-app.listen(3001, ()=>{
+app.get('/', (req, res) => {
+    res.send(str)
+})
+
+app.listen(3001, () => {
     console.log("server is running on port 3001")
 })
