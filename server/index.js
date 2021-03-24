ff6596a085ed488d905aed7eb8b4f30cf8127d67
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-24 21:59:53 +0400

    deploy server on heroku

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const deleteRoute = require('./routes/deleteRoute')
 app.use(cors())
 app.use(express.json());
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
